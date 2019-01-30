e7ad3ee3d180ca10f52df006ac104a7f0da34a45
TheEpicPenguin13 <TheEpicPenguin13@users.noreply.github.com>
2019-01-29 16:13:24 -0800

    Create Canvas
    Creating the canvas!

---
+++ src/main.js
@@ -0,0 +1,14 @@
+//-- CONSTANTS --\\
+const WIDTH = 600;
+const HEIGHT = 800;
+
+var setup = function()
+{
+    createCanvas(WIDTH, HEIGHT);
+    background(51);
+}
+
+var draw = function()
+{
+    
+}
