55d5ed64d7224f049f249e1d81389b220ee42ddb
TheEpicPenguin13 <TheEpicPenguin13@users.noreply.github.com>
2019-01-29 17:03:08 -0800

    Add Key Input
    Added Key Input to the game.

--- src/Player.js
+++ src/Player.js
@@ -3,6 +3,8 @@ class Player {
         this.x = x;
         this.y = y;
 
+        this.speed = 5;
+
         this.width;
         this.height;
     }
@@ -12,5 +14,16 @@ class Player {
 
         this.width = width;
         this.height = height;
+    }
+
+    update() {
+        // Check key input
+        if (keyCode === RIGHT_ARROW) {
+            this.speed = 5;
+            this.x += this.speed;
+        } else if (keyCode === LEFT_ARROW) {
+            this.speed = 5;
+            this.x -= this.speed;
+        }
     }
 }
--- src/main.js
+++ src/main.js
@@ -8,12 +8,13 @@ player = new Player(WIDTH / 2 - 50, HEIGHT - 20);
 var setup = function()
 {
     createCanvas(WIDTH, HEIGHT);
-    background(120);
     noStroke();
 }
 
 var draw = function()
 {
+    background(120);
     fill(60, 40, 40);
     player.draw(100, 20);
+    player.update();
 }
